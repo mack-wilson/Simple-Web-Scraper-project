f7d6a52386b75931557bb37b7f3d78191f93b333
mack-wilson <93164203+mack-wilson@users.noreply.github.com>
2023-04-26 17:54:39 -0700

    just switching IDE
    committing because im moving into vscode

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+""" This script will be a simple web scraper.
+    I am setting it's default URL to https://scrapethissite.com/pages/
+    and scanning for h3 with class "page-title"
+""""
 
+import requests
+from bs4 import BeautifulSoup
+
+
+def fetch_content(url):
+ 
